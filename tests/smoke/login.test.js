fa63fe5968eb20f16f86bbf3dc76a46e0ec4936b
EthemKesim <kesimethem@gmail.com>
2025-04-23 10:52:14 +0200

    login.test.js

--- tests/smoke/login.test.js
+++ tests/smoke/login.test.js
@@ -4,7 +4,7 @@ const { LoginPage } = require('../../pages/loginPage');
 test('Smoke Test: The user can log in.', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await page.goto('https://the-internet.herokuapp.com/login'); 
-    await loginPage.login('tomsmith', 'SuperSecretPassword!');  
+    await loginPage.login('emre', 'ethem123');  
     await expect(page).toHaveURL('https://the-internet.herokuapp.com/secure'); 
     await expect(page.locator('.flash.success')).toContainText('You logged into a secure area!');
 });
